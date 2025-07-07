2141c896b19979b1da69511359b28faf10831262
nologytech <hj045021@gmail.com>
2025-07-08 00:53:36 +0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Auto redirect to Page 1
-window.onload = () => {
-  setTimeout(() => {
-    window.location.href = "https://unitconverter4u.github.io/Unitconverter/";
-  }, 1000); // redirect after 1 second
-};
