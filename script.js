b800aae5ad743d8c11d9cd9cf92b70b45a328dd7
henry123455 <hj045021@gmail.com>
2025-07-07 19:28:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Auto redirect to Page 1
+window.onload = () => {
+  setTimeout(() => {
+    window.location.href = "https://unitconverter4u.github.io/Unitconverter/";
+  }, 1000); // redirect after 1 second
+};
